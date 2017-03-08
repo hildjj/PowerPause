a0dd330d44edf301148a37675bfe175c07b99c1e
Joe Hildebrand <joe-github@cursive.net>
2017-03-08 11:05:45 -0700

    Initial Commit

---
+++ PowerPause/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PowerPause
+//
+//  Created by Joe Hildebrand on 3/8/17.
+//  Copyright © 2017 Joe Hildebrand. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
